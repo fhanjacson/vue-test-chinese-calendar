4b5b4741b47193e1726d6d31683918f75cfcbc71
fhanjacson <fhan.jacson@gmail.com>
2022-06-19 20:28:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-dq7nws
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-dq7nws)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ todayDate.toDateString() }}</h1>
+    <h2>
+      Chinese: Tanggal {{ chineseDate.day }} Bulan {{ chineseDate.month }}
+    </h2>
+
+    <input type="date" v-model="selectedDate">
+    <button v-on:click=""> Select </button>
+    <h2 v>
+      Chinese: Tanggal {{ selectChineseDate.day }} Bulan {{ selectChineseDate.month }}
+    </h2>
+  </div>
+</template>
+
+<script>
+import { CalendarChinese } from 'date-chinese';
+
+export default {
+  todayDate: null,
+  chineseDate: null,
+  selectedDate: null,
+  selectChineseDate: null,
+  created() {
+    const calendarChinese = new CalendarChinese();
+    this.todayDate = new Date('2022 June 19');
+    this.chineseDate = calendarChinese.fromDate(this.todayDate);
+    console.log(this.chineseDate);
+  },
+  methods: {
+    selectDate() {
+      this.selectChineseDate = calendarChinese.fromDate(this.selectDate)
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
